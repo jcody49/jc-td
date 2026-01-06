1849eb513fa0e71f4c7454955d119b990f0b0593
jcody49 <123919470+jcody49@users.noreply.github.com>
2026-01-06 15:03:30 -0700

    adds level 2 assets for cannon, tank, acid, and frost

--- towers/CannonTower.js
+++ towers/CannonTower.js
@@ -9,10 +9,10 @@ export class CannonTower extends Tower {
             upgradeCosts: [50, 50, 400, 500, 4000],
             levelData: {
                 1: { damage: 55, range: 115, fireRate: 60, sprite: "cannon.png" },
-                2: { damage: 65, range: 120, fireRate: 58, sprite: "cannon2.png" },
-                3: { damage: 80, range: 125, fireRate: 55, sprite: "cannon3.png" },
-                4: { damage: 110, range: 130, fireRate: 50, sprite: "cannon4.png" },
-                5: { damage: 150, range: 140, fireRate: 45, sprite: "cannon5.png" }
+                2: { damage: 75, range: 115, fireRate: 58, sprite: "cannon2.png" },
+                3: { damage: 90, range: 125, fireRate: 55, sprite: "cannon3.png" },
+                4: { damage: 120, range: 130, fireRate: 50, sprite: "cannon4.png" },
+                5: { damage: 170, range: 140, fireRate: 45, sprite: "cannon5.png" }
               }              
         });
     }
