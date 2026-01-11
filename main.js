7320e91f8734a4691ce791d82bc42928e1a37100
jcody49 <123919470+jcody49@users.noreply.github.com>
2026-01-10 23:58:44 -0700

    splits out pathing.js

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ import {
   getHoveredEnemy,
   getTowerAtPosition
 } from './utils.js';
+import {
+  pathCells,
+  pathOccupied,
+  buildPath
+} from './pathing.js';
+
 
 
 
@@ -69,31 +75,8 @@ const gridSizeX = canvas.width / gridCols;
 const gridSizeY = canvas.height / gridRows;
 const gridSize = Math.min(gridSizeX, gridSizeY);
 
-/**********************
- * PATHING
- **********************/
-const pathCells = [
-  { col: 0, row: 7 }, { col: 1, row: 7 }, { col: 2, row: 7 }, { col: 3, row: 7 }, { col: 4, row: 7 },
-  { col: 4, row: 6 }, { col: 4, row: 5 }, { col: 4, row: 4 }, { col: 4, row: 3 },
-  { col: 5, row: 3 }, { col: 6, row: 3 },
-  { col: 6, row: 4 }, { col: 6, row: 5 }, { col: 6, row: 6 }, { col: 6, row: 7 }, { col: 6, row: 8 },
-  { col: 6, row: 9 }, { col: 6, row: 10 }, { col: 6, row: 11 },
-  { col: 7, row: 11 }, { col: 8, row: 11 },
-  { col: 8, row: 10 }, { col: 8, row: 9 }, { col: 8, row: 8 }, { col: 8, row: 7 }, { col: 8, row: 6 },
-  { col: 8, row: 5 }, { col: 8, row: 4 }, { col: 8, row: 3 },
-  { col: 9, row: 3 }, { col: 10, row: 3 },
-  { col: 10, row: 4 }, { col: 10, row: 5 }, { col: 10, row: 6 }, { col: 10, row: 7 },
-  { col: 11, row: 7 }, { col: 12, row: 7 }, { col: 13, row: 7 }, { col: 14, row: 7 },
-  { col: 15, row: 7 }, { col: 16, row: 7 }, { col: 17, row: 7 },
-  { col: 18, row: 7 }, { col: 19, row: 7 }, { col: 20, row: 7 },
-  { col: 21, row: 7 }, { col: 22, row: 7 }, { col: 23, row: 7 }, { col: 24, row: 7 }
-];
+const path = buildPath(pathCells, gridSize);
 
-const pathOccupied = pathCells.map(c => `${c.col},${c.row}`);
-const path = pathCells.map(c => ({
-  x: c.col * gridSize + gridSize / 2,
-  y: c.row * gridSize + gridSize / 2
-}));
 
 /***********************
  * INIT HUD
