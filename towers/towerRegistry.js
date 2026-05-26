409d70ddddbf9634971df3c1ae1149e40005603b
jcody49 <123919470+jcody49@users.noreply.github.com>
2026-05-26 09:12:47 -0600

    Fix tower import paths for GitHub Pages

--- towers/towerRegistry.js
+++ towers/towerRegistry.js
@@ -1,10 +1,10 @@
-import { CannonTower } from "./cannonTower.js";
-import { FrostTower } from "./frostTower.js";
-import { AcidTower } from "./acidTower.js";
-import { TankTower } from "./tankTower.js";
-import { AntiAirTower } from './AntiAir.js';
-import { DetectionTower } from "./DetectionTower.js";
-import { BoosterTower } from "./BoosterTower.js";
+import { CannonTower } from "./towers/CannonTower.js";
+import { FrostTower } from "./towers/FrostTower.js";
+import { AcidTower } from "./towers/AcidTower.js";
+import { TankTower } from "./towers/TankTower.js";
+import { AntiAirTower } from "./towers/AntiAir.js";
+import { DetectionTower } from "./towers/DetectionTower.js";
+import { BoosterTower } from "./towers/BoosterTower.js";
 
 export const TOWER_REGISTRY = {
     cannon: CannonTower,
