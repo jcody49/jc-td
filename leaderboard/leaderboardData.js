aa794bd0bfc47f20a1c94e49b8de861ac48a4a9d
jcody49 <123919470+jcody49@users.noreply.github.com>
2026-05-28 16:07:44 -0600

    adds new scores into leaderboard data

--- leaderboard/leaderboardData.js
+++ leaderboard/leaderboardData.js
@@ -1,7 +1,7 @@
 export const leaderboardData = [
+    { name: "Matt Cody", wave: 30, score: 3048 },
+    { name: "Emerson Lawrence", wave: 26, score: 2604 },
     { name: "JC$", wave: 30, score: 2580 },
-    { name: "Amanda Huggenkis", wave: 16, score: 1345 },
-    { name: "Mike Hunt", wave: 9, score: 900 },
     { name: "Seymour Butz", wave: 7, score: 720 },
     { name: "Hugh Jass", wave: 5, score: 530 }
   ];
