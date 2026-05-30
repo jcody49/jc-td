bbba50a8ae939579c04ce8c3f651912efc3547d8
jcody49 <123919470+jcody49@users.noreply.github.com>
2026-05-29 20:17:54 -0600

    all the scores are back where they should be

--- leaderboard/leaderboardData.js
+++ leaderboard/leaderboardData.js
@@ -1,7 +1,7 @@
 export const leaderboardData = [
+    { name: "JC$", wave: 30, score: 3090 },
     { name: "Matt Cody", wave: 30, score: 3048 },
     { name: "Emerson Lawrence", wave: 26, score: 2604 },
-    { name: "JC$", wave: 30, score: 2580 },
     { name: "Seymour Butz", wave: 7, score: 720 },
     { name: "Hugh Jass", wave: 5, score: 530 }
   ];
