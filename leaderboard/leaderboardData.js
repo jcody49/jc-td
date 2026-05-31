ac9f3684c43adaf3fc08f59f9049594af1319c0d
jcody49 <123919470+jcody49@users.noreply.github.com>
2026-05-31 16:05:03 -0600

    added axels highscore

--- leaderboard/leaderboardData.js
+++ leaderboard/leaderboardData.js
@@ -1,7 +1,7 @@
 export const leaderboardData = [
     { name: "JC$", wave: 30, score: 3090 },
+    { name: "Axel Learn", wave: 30, score: 3060 },
     { name: "Matt Cody", wave: 30, score: 3048 },
     { name: "Emerson Lawrence", wave: 26, score: 2604 },
-    { name: "Seymour Butz", wave: 7, score: 720 },
     { name: "Hugh Jass", wave: 5, score: 530 }
   ];
